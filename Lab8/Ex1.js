9e094f25e85f4ba78a1f0e71a1c70d86eaa24414
CliffordLeung24 <leungcl@hawaii.edu>
2021-10-06 13:06:13 -1000

    ss

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -2,13 +2,28 @@ require("./products_data.js");
 
 var num_products = 5;
 
-var count = 1;
-while (count <= num_products/2){
-    if (count > num_products/2){
-        console.log("That's enough!")
-        break;
+var count = 0;
+while (count < num_products){
+   count++;
+    if (count > num_products / 2 && count < (num_products *0.75)) {
+       console.log(`${eval('name'+ count)} is sold out.`);
+    }
+    else if (count > num_products/ 2){
+        console.log ("That's all we have!")
+        Process.exit ()
+    }
+    else { 
+    console.log(`${count}. ${eval('name' + count)}`);
+    }
+}
+
+
+
+var num_products = 5;
+for (var count = 1; eval("typeof name"+count) != 'undefined'; count++) {
+    if (count > num_products/2) {
+        console.log("That's enough!");
+        process.exit();
     }
     console.log(`${count}. ${eval('name' + count)}`);
-    count++;
 }
-console.log ("That's all we have!")
