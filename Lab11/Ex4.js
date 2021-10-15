3c7184d7f40ef135a2ec4beb8407af233c1a99f7
CliffordLeung24 <leungcl@hawaii.edu>
2021-10-15 13:06:37 -1000

    Update Ex4.js

--- Lab11/Ex4.js
+++ Lab11/Ex4.js
@@ -21,7 +21,7 @@ console.log("Bad input =", convertTemperature(212,"Q"));
 var attributes = "5;-3;0;xxx;7.5;13";
 var pieces = attributes.split(";");
 
-function isNonNegInt(q) {
+function isNonNegativeInteger(q, returnErrors = false) {
     //Validate that an input value is a non-negative integer
     // q is input string
    
@@ -29,9 +29,9 @@ function isNonNegInt(q) {
     if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
     if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
     if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-    return errors.length == 0;
+    return returnErrors ? errors : (errors.length == 0);
 }
 
 for (testVal in pieces){
-    console.log(testVal + ": " + pieces[testVal] + " " +isNonNegInt(pieces [testVal]));
+    console.log(testVal + ": " + pieces[testVal] + " " +isNonNegativeInteger(pieces [testVal], true));
 }
