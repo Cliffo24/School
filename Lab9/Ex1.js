a10fc99e89ba39b172f4df51b133818d874847ed
CliffordLeung24 <leungcl@hawaii.edu>
2021-10-20 11:32:09 -1000

    Create Ex1.js

---
+++ Lab9/Ex1.js
@@ -0,0 +1,10 @@
+step1 = 01;
+step2 = parseInt(step1/4);
+step3 = step2 + step1;
+step4 = 3; // Not Jan, so look at month before on table
+step6 = step4 + step3;
+step7 = step6 + 4;
+step8 = (step7-1);
+step9 = (step8/7);
+console.log(step9)
+02/04/2001
