6c05b3da8dd18cdfbe6ef984d62a973637928468
CliffordLeung24 <leungcl@hawaii.edu>
2021-10-20 11:37:43 -1000

    Update Ex1.js

--- Lab9/Ex1.js
+++ Lab9/Ex1.js
@@ -5,6 +5,11 @@ step4 = 3; // Not Jan, so look at month before on table
 step6 = step4 + step3;
 step7 = step6 + 4;
 step8 = (step7-1);
-step9 = (step8/7);
-console.log(step9)
-02/04/2001
+if (step8 <= 7){
+   console.log(step8/7)  
+}
+else{
+    console.log(step8)
+}
+
+//02/04/2001
