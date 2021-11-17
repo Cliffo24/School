e4baf99e15f37e08fc5503a66dee05cbbb319293
CliffordLeung24 <leungcl@hawaii.edu>
2021-11-16 17:51:40 -1000

    ss

--- Leung_Clifford_Assignment1/server.js
+++ Leung_Clifford_Assignment1/server.js
@@ -2,18 +2,18 @@
 var express = require('express');// require express
 var app = express();//require express
 var myParser = require("body-parser");//require body parser
-var data = require('./public/products_data'); //load products.json
-var products = data.products; //assign products with data
-var http = require('http');
+var data = require ('./public/products_data.js'); //load product_data.js
+var products = data.products; //Assign data to products
 
 
-var products = require('./public/products_data');
+var products = require('./products_data.js');
 products.forEach( (prod,i) => {prod.total_sold = 0});
 
-app.get("./public/products_data.js", function (request, response, next) {
+app.get("/products_data.js", function (request, response, next) {
     response.type('.js');
     var products_str = `var products = ${JSON.stringify(products)};`;
     response.send(products_str);
+    console.log(products_str)
  });
 
 
@@ -34,63 +34,13 @@ function isNonNegativeInteger(inputString, returnErrors = false) {
 }
 //Route to handle any request; also calls next
 app.all('*', function (request, response, next) {
-    console.log (request.method + ' to path ' + request.path);
+    //console.log (request.method + ' to path ' + request.path);
     next();
 });
 
 app.use(myParser.urlencoded({ extended: true }));
-//Rule to handle process_form request form products_display
-app.get("/purchase", function (request, response, next) {
-    let POST = request.body;
-    if(typeof POST['purchase_submit'] == 'undefined') {
-        console.log('No purchase form data');
-        next();
-    } 
+//Rule to handle process_form request form purchasing page
 
-    function display_invoice_table_rows() {
-        subtotal = 0;
-        str = '';
-        for (i = 0; i < products.length; i++) {
-            a_qty = 0;
-            if(typeof POST[`quantity${i}`] != 'undefined') {
-                a_qty = POST[`quantity${i}`];
-            }
-            if (a_qty > 0) {
-                // product row
-                extended_price =a_qty * products[i].price
-                subtotal += extended_price;
-                str += (`
-      <tr>
-        <td width="43%">${products[i].model}</td>
-        <td align="center" width="11%">${a_qty}</td>
-        <td width="13%">\$${products[i].price}</td>
-        <td width="54%">\$${extended_price}</td>
-      </tr>
-      `);
-            }
-        }
-        // Compute tax
-        tax_rate = 0.0575;
-        tax = tax_rate * subtotal;
-
-        // Compute shipping
-        if (subtotal <= 50) {
-            shipping = 2;
-        }
-        else if (subtotal <= 100) {
-            shipping = 5;
-        }
-        else {
-            shipping = 0.05 * subtotal; // 5% of subtotal
-        }
-
-        // Compute grand total
-        total = subtotal + tax + shipping;
-
-        return str;
-    }
-
-});
 app.use(express.static('./public'));
 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here
